140be3ebc0d1afb000060a2484804e646cb30cfb
Ayomide <ilesanmibry@gmail.com>
2024-07-08 17:35:00 +0100

    added vercel config

--- build/interfaces/user.interface.js
+++
@@ -1,2 +0,0 @@
-// user
-//# sourceMappingURL=user.interface.js.map
--- build/middlewares/validations/organisation.zod.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateOrgDataSchema = void 0;
-const zod_1 = require("zod");
-exports.CreateOrgDataSchema = zod_1.z.object({
-    name: zod_1.z.string().min(3),
-    description: zod_1.z.string(),
-});
-//# sourceMappingURL=organisation.zod.js.map
